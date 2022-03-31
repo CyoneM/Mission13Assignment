f46fe9d59241c9cbaf0373227d17f9cfc9a3af34
CyoneM <mtdavis32@gmail.com>
2022-03-30 23:47:59 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission13Assignment.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission13Assignment.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+    }
+}
